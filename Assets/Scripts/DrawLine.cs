d24e5209d9b4634c1902c8b06a7562f2ba82ec3e
minding2796 <seunghun9080@gmail.com>
2024-07-28 22:42:14 +0900

    DrawLine.cs Refactoring

--- Assets/Scripts/DrawLine.cs
+++ Assets/Scripts/DrawLine.cs
@@ -1,36 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Net.NetworkInformation;
 using UnityEngine;
 
 public class DrawLine : MonoBehaviour
 {
-    [SerializeField] private Camera mainCam;
     public GameObject linePrefab;
-    private LineRenderer lr;
-    private List<Vector2> points = new List<Vector2>();
-    
-    void Update()
+
+    private Camera _mainCam;
+    private LineRenderer _lr;
+
+    private void Start()
+    {
+        _mainCam = GetComponent<Camera>();
+    }
+
+    private void Update()
     {
         if (Input.GetMouseButtonDown(0))
-        {
-            GameObject go = Instantiate(linePrefab);
-            lr = go.GetComponent<LineRenderer>();
-            points.Add(mainCam.ScreenToWorldPoint(Input.mousePosition));
-            lr.positionCount = 1;
-            lr.SetPosition(0, points[0]);
-        }
-        else if (Input.GetMouseButton(0))
-        {
-            Vector2 pos = mainCam.ScreenToWorldPoint(Input.mousePosition);
-            
-            points.Add(pos);
-            lr.positionCount++;
-            lr.SetPosition(lr.positionCount-1,pos);
-        }
-        else if (Input.GetMouseButtonUp(0))
-        {
-            points.Clear();
-        }
+            (_lr = Instantiate(linePrefab).GetComponent<LineRenderer>()).positionCount = 0;
+        
+        if (Input.GetMouseButton(0))
+            _lr.SetPosition(_lr.positionCount++,_mainCam.ScreenToWorldPoint(Input.mousePosition) - Vector3.back * 10);
     }
 }
