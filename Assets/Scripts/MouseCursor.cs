0e8272b1430266d273721004c41b0e81c3c3aba1
oty0365 <163986958+oty0365@users.noreply.github.com>
2024-07-29 23:00:56 +0900

    Make Width

--- Assets/Scripts/MouseCursor.cs
+++ Assets/Scripts/MouseCursor.cs
@@ -1,9 +1,11 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MouseCursor : MonoBehaviour
 {
     [SerializeField] private Camera mainCam;
+    [SerializeField] private Slider slider;
     private SpriteRenderer sr;
     private void Start()
     {
@@ -14,5 +16,7 @@ public class MouseCursor : MonoBehaviour
     {
         sr.color = ColorSelector.GetColor();
         transform.position = mainCam.ScreenToWorldPoint(Input.mousePosition) + Vector3.forward * 10;
+        gameObject.transform.localScale = new Vector3(slider.value, slider.value, 1);
+
     }
 }
