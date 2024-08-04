bdeaab0ad39500b536bcb8583b26a0def07a3bbf
whitefish2n2 <whitefish_@naver.com>
2024-08-04 21:16:21 +0900

    생성한 오브젝트->Play scene PipeLine rework

--- Assets/Scripts/DrawLine.cs
+++ Assets/Scripts/DrawLine.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
@@ -15,6 +16,8 @@ public class DrawLine : MonoBehaviour
     private EdgeCollider2D _col;
     public static readonly List<GameObject> Objects = new();
     public static GameObject SavedObject;
+    public static List<GameObject> Animations = new();
+    
     
     private void Start()
     {
@@ -28,8 +31,29 @@ public class DrawLine : MonoBehaviour
             SavedObject = new GameObject();
             foreach (var o in Objects) o.transform.parent = SavedObject.transform;
             Objects.Clear();
-            DontDestroyOnLoad(SavedObject);
             SavedObject.transform.position = new Vector3(0, -10000);
+            if (!SavedObject) return;
+            SavedObject.transform.position = transform.position;
+            var list = new HashSet<Vector2>();
+            var children = SavedObject.GetComponentsInChildren<EdgeCollider2D>();
+            float radius = 0;
+            foreach (var c in children)
+            {
+                var line = c.GetComponent<LineRenderer>();
+                line.startWidth *= 0.1f;
+                line.endWidth *= 0.1f;
+                radius = Math.Max(line.startWidth / 2, radius);
+                foreach (var point in c.points) if (list.All(p => (p - point).magnitude > radius * 20)) list.Add(point);
+                Destroy(c);
+            }
+            var col = SavedObject.AddComponent<EdgeCollider2D>();
+            col.points = list.ToArray();
+            col.edgeRadius = radius;
+            SavedObject.transform.localScale = new Vector3(0.1f, 0.1f);
+            Destroy(GetComponent<SpriteRenderer>());
+            Destroy(GetComponent<CircleCollider2D>());
+            Animations.Add(SavedObject);
+            DontDestroyOnLoad(SavedObject);
             SceneManager.LoadScene("Play");
         }
         if (!UiManager.CanEdit) return;
