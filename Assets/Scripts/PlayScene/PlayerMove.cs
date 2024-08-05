f3f525725fe3a81cf9fecccd7431264ccbe324b3
minding2796 <seunghun9080@gmail.com>
2024-08-05 09:38:49 +0900

    Refactoring

--- Assets/Scripts/PlayScene/PlayerMove.cs
+++ Assets/Scripts/PlayScene/PlayerMove.cs
@@ -9,10 +9,11 @@ namespace PlayScene
     public class PlayerMove : MonoBehaviour
     {
         private int _horizontal;
+        private float _moveElapsedTime;
         private Rigidbody2D _rigid;
-        [SerializeField] private float speed;
         private Queue<GameObject> _moveObjectQueue;
-        private GameObject _defaultObj;
+        private GameObject _defaultObj, _movingObjectTemp;
+        [SerializeField] private float speed;
         [SerializeField] private float frameRate;
 
         private void Start()
@@ -25,7 +26,6 @@ namespace PlayScene
                 o.transform.position = transform.position;
                 _moveObjectQueue.Enqueue(o);
                 o.SetActive(false);
-                
             }
             _defaultObj = gameObject.transform.GetChild(0).gameObject;
             _defaultObj.SetActive(true);
@@ -35,60 +35,46 @@ namespace PlayScene
 
         private void Update()
         {
-            _horizontal = Input.GetKey(KeyCode.A) ? Input.GetKey(KeyCode.D) ? 0 : -1 : Input.GetKey(KeyCode.D) ? 1 : 0;
+            _horizontal = Input.GetKey(KeyCode.A)
+                ? Input.GetKey(KeyCode.D) ? 0 : -1
+                : Input.GetKey(KeyCode.D) ? 1 : 0;
         }
 
         private void FixedUpdate()
         {
             _rigid.velocity = new Vector2(_horizontal * speed, _rigid.velocity.y);
-            var position = transform.position;
-            transform.position = new Vector3(Math.Clamp(position.x, -8f, 8f), position.y);
         }
-
-        private GameObject _movingObjectTemp;
-        private void NextFrame()
-        {
-            if (_horizontal == 1)
-            { 
-                _movingObjectTemp.SetActive(false);
-                if (_movingObjectTemp != _defaultObj)
-                {
-                    _moveObjectQueue.Enqueue(_movingObjectTemp);
-                    _movingObjectTemp = _moveObjectQueue.Dequeue();
-                    _movingObjectTemp.SetActive(true);
-                }
-                _movingObjectTemp.transform.rotation = Quaternion.Euler(0, 0, 0);
-            }
-            else if (_horizontal == -1)
-            {
-                _movingObjectTemp.SetActive(false);
-                if (_movingObjectTemp != _defaultObj)
-                {
-                    _moveObjectQueue.Enqueue(_movingObjectTemp);
-                    _movingObjectTemp = _moveObjectQueue.Dequeue();
-                    _movingObjectTemp.SetActive(true);
-                }
-                _movingObjectTemp.transform.rotation = Quaternion.Euler(0, 180, 0);
-            }
-            else
-            {
-                if (_movingObjectTemp != _defaultObj)
-                {
-                    _moveObjectQueue.Enqueue(_movingObjectTemp);
-                    _movingObjectTemp.SetActive(false);
-                }
-                _movingObjectTemp = _defaultObj;
-                _defaultObj.SetActive(true);
-            }
-        }
-
-        private float _moveElapsedTime;
         private IEnumerator MovingTick()
         {
             while (true)//그냥 실행시 반복
             {
                 yield return new WaitForSeconds(frameRate);
-                NextFrame();
+                switch (_horizontal)
+                {
+                    case not 0:
+                    {
+                        _movingObjectTemp.SetActive(false);
+                        if (_movingObjectTemp != _defaultObj)
+                        {
+                            _moveObjectQueue.Enqueue(_movingObjectTemp);
+                            _movingObjectTemp = _moveObjectQueue.Dequeue();
+                        }
+                        _movingObjectTemp.SetActive(true);
+                        _movingObjectTemp.transform.rotation = Quaternion.Euler(0, _horizontal == 1 ? 0 : 180, 0);
+                        break;
+                    }
+                    default:
+                    {
+                        if (_movingObjectTemp != _defaultObj)
+                        {
+                            _moveObjectQueue.Enqueue(_movingObjectTemp);
+                            _movingObjectTemp.SetActive(false);
+                        }
+                        _movingObjectTemp = _defaultObj;
+                        _defaultObj.SetActive(true);
+                        break;
+                    }
+                }
             }
         }
     }
